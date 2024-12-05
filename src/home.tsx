c79214ae87e50f401dbf69ba66a9bb0ba59983ff
Jairo Cabrera <jairocabrera87@hotmail.com>
2024-12-05 08:15:26 -0500

    add images

--- src/home.tsx
+++ src/home.tsx
@@ -1,6 +1,8 @@
 import Body from './body/Body';
 import Footer from './footer/Footer';
 import Header from './header/Header';
+import phone from './assets/phone.png';
+import map from './assets/map.png';
 
 interface Props {
     indexSelected: number
@@ -10,7 +12,7 @@ interface Props {
 export default function Home({ indexSelected, logo }: Props) {
     return (
         <>
-            <Header indexSelected={indexSelected} logo={logo} phone={'./assets/phone.png'} map={'./assets/map.png'} />
+            <Header indexSelected={indexSelected} logo={logo} phone={phone} map={map} />
             <Body />
             <Footer indexSelected={indexSelected} />
         </>
